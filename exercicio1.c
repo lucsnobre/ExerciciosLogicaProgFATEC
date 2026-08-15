6ec6a7fca9f3f025b36a7f8db89ab6e2bc207e26
Lucas Nobre <lucasfilbeto@gmail.com>
2026-08-15 10:38:36 -0300

    COMMIT FEITO

---
+++ exercicio1.c
@@ -0,0 +1,20 @@
+/*  exercício 1 - verificar se um num é par ou ímpar */
+
+#include <stdio.h>
+
+int main (void)
+{
+    int num;
+    
+    printf("Digite um número inteiro:" );
+    scanf("%d", &num);
+    
+    if (num % 2 == 0) {
+        printf("%d é par \n" , num);
+    }else {
+        printf("%d é ímpar\n", num);
+    }
+    
+    return 0;
+    
+}
