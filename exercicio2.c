7f52817f83c2ee068ea5e25bcf5fd85d62a085f2
Lucas Nobre <lucasfilbeto@gmail.com>
2026-08-27 18:36:29 -0300

    commit do ex 2

--- exercicio2.c
+++ exercicio2.c
@@ -1,1 +1,18 @@
+/*ex 2 - verificação de maioridade*/
+#include <stdio.h>
+
+int main( void )
+{
+    int idade;
+
+    printf( "Digite sua idade: " );
+    scanf( "%d", &idade );
+
+    if ( idade >= 18 ) {
+        printf( "Você é maior de idade\n" );
+    }
+
+    if ( idade < 18 ) {
+        printf( "Você é menor de idade\n" );
+    }
 
